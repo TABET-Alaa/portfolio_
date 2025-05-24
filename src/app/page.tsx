0659fa7e794e289616d057cea09d6b12c8701353
Alaaddine-ta <alae.tabet02@gmail.com>
2025-05-24 12:01:59 +0100

    update the version3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,7 +84,7 @@ export default function Home() {
         <Flex fillWidth gap="24" mobileDirection="column">
           <Flex flex={1} paddingLeft="l" paddingTop="24">
             <Heading as="h2" variant="display-strong-xs" wrap="balance">
-              Latest from the blog 1
+              LATEST FROM ALAA AYMAN SAAD
             </Heading>
           </Flex>
           <Flex flex={3} paddingX="20">
